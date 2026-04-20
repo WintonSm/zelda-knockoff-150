da35ebad04afe700f9676e8f15515a05f0440bb1
SmithQ4723 <wintonmusic@proton.me>
2026-04-20 16:35:33 -0500

    Create BehaviorController.java

---
+++ BehaviorController.java
@@ -0,0 +1,24 @@
+
+/*
+ * An intermediary class for storing and transferring certain data between the view and model logic.
+ * @author Quenton Smith
+ */
+public class BehaviorController {
+	private int floor;
+
+	BehaviorController() {
+		this.floor = 1;
+	}
+
+	//Floor methods
+	public int getFloor() {
+		return floor;
+	}
+
+	public void setFloor(int floor) {
+		this.floor = floor;
+	}
+	
+	
+	
+}
