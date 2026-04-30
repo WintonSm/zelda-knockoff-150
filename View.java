a13284d8acdd8ae5db57ab538b774ba70f14003f
SmithQ4723 <wintonmusic@proton.me>
2026-04-29 21:01:42 -0500

    Health Potion Button Update and Win/Loss Screen Swap

--- View.java
+++ View.java
@@ -1,4 +1,6 @@
 import java.awt.Color;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -123,7 +125,11 @@ public class View {
 		DrawingPanel statPanel = this.panels[2];
 		statPanel.removeAll();
 		
-		JButton healingPotionBtn = new JButton("Use Health Potion");
+		ImageIcon healingPotion = new ImageIcon("new-sprites/Items/Health-Potion-Large.png");
+		JButton healingPotionBtn = new JButton(healingPotion);
+		healingPotionBtn.setBorderPainted(false);
+		healingPotionBtn.setContentAreaFilled(false);
+		healingPotionBtn.setOpaque(false);
 		
 		int[] dims = statPanel.scale(.1, .4, .8, .1);
 		healingPotionBtn.setBounds(dims[0], dims[1], dims[2], dims[3]);
